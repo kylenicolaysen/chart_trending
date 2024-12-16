ed95cecd6ac67975d04ec51273992fa1a8a7f210
Kyle Nicolaysen <kylenicolaysen@gmail.com>
2024-12-15 16:50:19 -0800

    header errors?

--- mysite/settings.py
+++ mysite/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('DJANGO_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['charttrending.com', '127.0.0.1', 'localhost', '137.184.178.103']
+ALLOWED_HOSTS = ['*', 'charttrending.com', '127.0.0.1', 'localhost', '137.184.178.103']
 
 
 # Application definition
