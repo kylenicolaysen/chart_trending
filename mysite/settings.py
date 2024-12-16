233f86bf9083b02c506bf9ec62f92355e197ec51
Kyle Nicolaysen <kylenicolaysen@gmail.com>
2024-12-15 17:14:04 -0800

    trying to fix invalid header error

--- mysite/settings.py
+++ mysite/settings.py
@@ -131,3 +131,5 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
