b90cd0dac60496451ef66e8dfc0222a63ef411c3
Kyle Nicolaysen <kylenicolaysen@gmail.com>
2024-12-15 18:08:12 -0800

    fixing 403 error on voting

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = Fa
+DEBUG = False
 
 ALLOWED_HOSTS = [os.getenv('IP_ADDRESS')]
 # 'charttrending.com', '127.0.0.1', 'localhost', '137.184.178.103'
