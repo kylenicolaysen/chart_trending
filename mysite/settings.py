40eed4aaebdb684c2a8902fc35ded015130d16d2
Kyle Nicolaysen <kylenicolaysen@gmail.com>
2024-12-15 16:39:46 -0800

    fixed ip address

--- mysite/settings.py
+++ mysite/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('DJANGO_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['charttrending.com', '64.23.240.149']
+ALLOWED_HOSTS = ['charttrending.com', '137.184.178.103']
 
 
 # Application definition
