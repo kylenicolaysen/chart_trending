667e6356e5617bc7ba4bcb274614f69dd3b265b9
Kyle Nicolaysen <kylenicolaysen@gmail.com>
2024-12-15 18:30:54 -0800

    fixing 403 error on voting

--- polls/views.py
+++ polls/views.py
@@ -25,19 +25,21 @@ class IndexView(generic.ListView):
     def get_queryset(self):
         return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
-class DetailView(generic.DetailView):
-    model = Question
-    template_name = 'polls/detail.html'
+# class DetailView(generic.DetailView):
+#     model = Question
+#     template_name = 'polls/detail.html'
 
-# def results(request, question_id):
-#     question = get_object_or_404(Question, pk=question_id)
-#     return render(request, 'polls/results.html', {'question': question})
+def results(request, question_id):
+    print(request.get_host())
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
 
 def vote(request, question_id):
+    print(request.get_host())
     question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST["choice"])
