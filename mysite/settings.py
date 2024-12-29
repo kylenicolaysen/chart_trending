bff712f7a34c6672e54d4ce13a79638f5215378c
Kyle Nicolaysen <kylenicolaysen@gmail.com>
2024-12-28 23:37:00 -0800

    trying to fix static files in prod

--- mysite/settings.py
+++ mysite/settings.py
@@ -125,7 +125,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'static'),
+    BASE_DIR / 'charts' / 'static',
 ]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
